b49d5ca5d14b3a3cb5a67d08b72067a88c08d1ce
DanielDev87 <danielhymtj@icloud.com>
2025-09-14 14:36:34 -0500

    Ajustando la configuración para aws

---
+++ applications/app-service/src/main/java/co/com/bancolombia/config/AwsConfig.java
@@ -0,0 +1,33 @@
+package co.com.bancolombia.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+
+@Configuration
+public class AwsConfig {
+
+    @Value("${cloud.aws.region.static}")
+    private String awsRegion;
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String awsAccessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String awsSecretKey;
+
+    @Bean
+    public SqsAsyncClient sqsAsyncClient() {
+        return SqsAsyncClient.builder()
+                .region(Region.of(awsRegion))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(awsAccessKey, awsSecretKey)
+                ))
+                .build();
+    }
+
+}
